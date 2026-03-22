9e90616120689c79b60b88e3d04ad2aaf52eedc4
Spearking91 <baninacquah@gmail.com>
2026-03-22 12:02:18 +0000

    feat: create AdminDashboard component with dynamic charts and metrics

--- app/(pages)/CheckoutPage/page.tsx
+++ app/(pages)/CheckoutPage/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Lottie from "lottie-react";
 import dynamic from "next/dynamic";
+import LoadingLogo from "@/public/lottie/loading2.json"
 
 const CheckoutForm = dynamic(() => import("./CheckoutForm"), {
   ssr: false,
   loading: () => (
     <div className="min-h-[70vh] flex items-center justify-center">
-      <span className="loading loading-spinner loading-lg"></span>
+      <Lottie animationData={LoadingLogo}>
     </div>
   ),
 });
