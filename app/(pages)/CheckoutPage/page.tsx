ec68f519d73bd2ab7acfff833a918a12fb186054
Spearking91 <baninacquah@gmail.com>
2026-03-22 12:03:59 +0000

    fix: correct syntax errors in CheckoutPage component

--- app/(pages)/CheckoutPage/page.tsx
+++ app/(pages)/CheckoutPage/page.tsx
@@ -2,13 +2,13 @@
 
 import Lottie from "lottie-react";
 import dynamic from "next/dynamic";
-import LoadingLogo from "@/public/lottie/loading2.json"
+import LoadingLogo from "@/public/lottie/loading2.json";
 
 const CheckoutForm = dynamic(() => import("./CheckoutForm"), {
   ssr: false,
   loading: () => (
     <div className="min-h-[70vh] flex items-center justify-center">
-      <Lottie animationData={LoadingLogo}>
+      <Lottie animationData={LoadingLogo} />
     </div>
   ),
 });
